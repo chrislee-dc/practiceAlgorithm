316026c6d373e6bdb2c953bc3dedca4e5667942b
Chris Lee <chrislee.dc.developer@gmail.com>
2025-03-26 23:40:27 -0600

    ..

---
+++ NeetCode150/4. Binary Search/2. Search a 2D Matrix.js
@@ -0,0 +1,54 @@
+// Search a 2D Matrix
+// You are given an m x n 2-D integer array matrix and an integer target.
+
+// Each row in matrix is sorted in non-decreasing order.
+// The first integer of every row is greater than the last integer of the previous row.
+// Return true if target exists within matrix or false otherwise.
+
+// Can you write a solution that runs in O(log(m * n)) time?
+
+class Solution {
+  /**
+   * @param {number[][]} matrix
+   * @param {number} target
+   * @return {boolean}
+   */
+  searchMatrix(matrix, target) {
+    let ROWS = matrix.length;
+    let COLS = matrix[0].length;
+    let l = 0;
+    let r = ROWS * COLS;
+
+    while (l < r) {
+      let m = l + Math.floor((r - l) / 2); // 6
+      let rows = Math.floor(m / COLS);
+      let cols = Math.floor(m % COLS); // 2
+      if (matrix[rows][cols] > target) {
+        r = m - 1;
+      } else if (matrix[rows][cols] < target) {
+        l = m + 1;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+const a = new Solution().searchMatrix(
+  [
+    [1, 2, 4, 8],
+    [10, 11, 12, 13],
+    [14, 20, 30, 40],
+  ],
+  10
+);
+console.log("🚀 ~ a: 2", a);
+
+// Input: matrix = [[1,2,4,8],[10,11,12,13],[14,20,30,40]], target = 10
+
+// Output: true
+
+// Input: matrix = [[1,2,4,8],[10,11,12,13],[14,20,30,40]], target = 15
+
+// Output: false
