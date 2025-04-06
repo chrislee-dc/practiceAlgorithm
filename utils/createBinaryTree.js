90825ce910c75ab218da81d7f1d860d821aab4ac
Daechul <dlee@nuundigital.com>
2025-04-06 10:22:51 -0600

    //

---
+++ utils/createBinaryTree.js
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class TreeNode {
+  constructor(val = 0, left = null, right = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+function createBinaryTree(arr) {
+  if (!arr.length || arr[0] === null) return null;
+
+  const root = new TreeNode(arr[0]);
+  const queue = [root];
+  let i = 1;
+
+  while (queue.length && i < arr.length) {
+    const current = queue.shift();
+
+    if (i < arr.length && arr[i] !== null) {
+      current.left = new TreeNode(arr[i]);
+      queue.push(current.left);
+    }
+    i++;
+
+    if (i < arr.length && arr[i] !== null) {
+      current.right = new TreeNode(arr[i]);
+      queue.push(current.right);
+    }
+    i++;
+  }
+
+  return root;
+}
+
+module.exports = createBinaryTree;
