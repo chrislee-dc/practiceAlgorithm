4aea8c718ff09035a765cd49926d0111c6a923e6
Daechul <dlee@nuundigital.com>
2025-04-21 21:27:42 -0600

    ..

---
+++ NeetCode150/14. 1-D Dynamic Programming/1. Climbing Stairs[neetCodeSolutionIsBetter_250420].js
@@ -0,0 +1,27 @@
+class Solution {
+  /**
+   * @param {number} n
+   * @return {number}
+   */
+  climbStairs(n) {
+    let one = 1;
+    let two = 1;
+
+    for (let i = 0; i < n - 1; i++) {
+      let temp = one;
+      one = one + two;
+      two = temp;
+    }
+    return one;
+  }
+}
+
+new Solution().climbStairs(4);
+
+/**
+ * 1111
+ * 112
+ * 121
+ * 211
+ * 22
+ */
