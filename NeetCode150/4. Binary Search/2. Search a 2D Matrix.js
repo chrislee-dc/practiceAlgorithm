0f1b268603e2e156a4b262847c1b98809fda60a0
Chris Lee <chrislee.dc.developer@gmail.com>
2025-05-03 15:20:37 -0600

    ..

--- NeetCode150/4. Binary Search/2. Search a 2D Matrix.js
+++ NeetCode150/4. Binary Search/2. Search a 2D Matrix.js
@@ -20,15 +20,16 @@ class Solution {
     let r = ROWS * COLS - 1;
 
     while (l <= r) {
-      let m = l + Math.floor((r - l) / 2); // 6
-      let rows = Math.floor(m / COLS);
-      let cols = Math.floor(m % COLS); // 2
-      if (matrix[rows][cols] > target) {
-        r = m - 1;
-      } else if (matrix[rows][cols] < target) {
-        l = m + 1;
-      } else {
+      let mid = Math.floor((l + r) / 2);
+      let newR = Math.floor(mid / COLS);
+      let newC = mid % COLS;
+
+      if (target === matrix[newR][newC]) {
         return true;
+      } else if (target < matrix[newR][newC]) {
+        r = mid - 1;
+      } else {
+        l = mid + 1;
       }
     }
     return false;
