2908bd23817f7eceb448264587d46b0b06cbcd3a
Daechul <chrislee.dc.developer@gmail.com>
2026-08-27 21:21:43 -0600

    debounce

--- frontend/debounce_260827.js
+++ frontend/debounce_260827.js
@@ -2,6 +2,7 @@
  * @param {(...args: Array<unknown>) => unknown} func
  * @param {number} wait
  * @returns {(...args: Array<unknown>) => void}
+ *
  */
 export default function debounce(func, wait) {
     let timeoutId = null;
